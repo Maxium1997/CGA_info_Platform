b94acdf43d1440bd59a0216f2e96e7b18f3f11e6
reSetRoot <wei860925@gmail.com>
2021-03-31 01:04:11 +0800

    Edited the sop models.

--- sop/models.py
+++ sop/models.py
@@ -5,10 +5,22 @@ from django.db import models
 from law.models import Article
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=155, null=False, blank=False)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+
 class Case(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     content = models.TextField()
-    related_laws = models.ManyToManyField(Article)
-    notice_items = models.TextField()
+    notice = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
+    related_laws = models.ManyToManyField(Article)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
