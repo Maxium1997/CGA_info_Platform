77010d4e091a945fb889fdf3c4c87251daa019dc
reSetRoot <wei860925@gmail.com>
2021-03-30 11:58:06 +0800

    Completed the create page layout change.

--- law/forms.py
+++ law/forms.py
@@ -6,8 +6,13 @@ from law.models import Chapter, Article
 class ChapterCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ChapterCreateForm, self).__init__(*args, **kwargs)
-        self.fields['source_url'].required = False
-        self.fields['number'] = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': self.initial.get('act').chapter_set.count()+1}))
+        self.fields['number'] = forms.IntegerField(required=True,
+                                                   widget=forms.NumberInput(attrs={'placeholder': self.initial.get('act').chapter_set.count()+1,
+                                                                                   'class': 'form-control'}))
+        self.fields['name'] = forms.CharField(required=True,
+                                              widget=forms.TextInput(attrs={'class': 'form-control'}))
+        self.fields['source_url'] = forms.URLField(required=False,
+                                                   widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     def save(self, commit=True):
         chapter = super(ChapterCreateForm, self).save(commit=False)
@@ -25,9 +30,16 @@ class ArticleCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ArticleCreateForm, self).__init__(*args, **kwargs)
         self.fields['chapter'] = forms.ModelChoiceField(queryset=self.initial.get('act').chapter_set.all(),
-                                                        required=False)
-        self.fields['priority'] = forms.CharField(widget=forms.TextInput(attrs={'placeholder': self.initial.get('act').article_set.count()+1}))
-        self.fields['number'] = forms.CharField(widget=forms.TextInput(attrs={'placeholder': self.initial.get('act').article_set.count()+1}))
+                                                        required=False,
+                                                        widget=forms.Select(attrs={'class': 'form-control'}))
+        self.fields['priority'] = forms.CharField(widget=forms.TextInput(attrs={'placeholder': self.initial.get('act').article_set.count()+1,
+                                                                                'class': 'form-control'}))
+        self.fields['number'] = forms.CharField(widget=forms.TextInput(attrs={'placeholder': self.initial.get('act').article_set.count()+1,
+                                                                              'class': 'form-control'}))
+        self.fields['content'] = forms.CharField(required=True,
+                                                 widget=forms.Textarea(attrs={'class': 'form-control'}))
+        self.fields['source_url'] = forms.URLField(required=True,
+                                                   widget=forms.URLInput(attrs={'class': 'form-control'}))
 
     def save(self, commit=True):
         article = super(ArticleCreateForm, self).save(commit=False)
